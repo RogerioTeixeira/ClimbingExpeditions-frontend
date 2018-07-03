a53307311d4c71816087fedfd0bd6f94089e5544
RogerioTeixeira <rogerio.teixeira@hotmail.it>
2018-07-03 22:43:49 +0200

    improve profile info component

--- src/app/profile/components/user-info/user-info.component.ts
+++ src/app/profile/components/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'clex-user-info',
@@ -9,6 +9,8 @@ export class UserInfoComponent implements OnInit {
   @Input() imageUrl: string;
   @Input() name: string;
   @Input() location: string;
+
+  @Output() clickUpdate: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {}
 
--- src/app/profile/containers/profile.component.ts
+++ src/app/profile/containers/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { UserInfoEditComponent } from '../components/user-info-edit/user-info-edit.component';
 
 @Component({
   selector: 'clex-profile',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(UserInfoEditComponent, {minWidth: 744 , maxWidth: 'calc(100vh - 64px)'});
   }
 
 }
